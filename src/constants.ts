27488723fdfac2e728842fbe6853df6652b17812
Nomewei <benjamin.besa@gmail.com>
2025-07-28 15:38:04 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 import { Plan } from './types';
 
 export const PLANS: Plan[] = [
-  { name: "Personal", slots: 1, price: 500 },
-  { name: "Dúo", slots: 2, price: 14990 },
-  { name: "Familiar", slots: 5, price: 28990 },
+  { name: "Personal", Contacto: 1, price: 8990 },
+  { name: "Dúo", Contactos: 2, price: 14990 },
+  { name: "Familiar", Contactos: 5, price: 28990 },
 ];
 
 export const RECOMMENDED_PLAN_NAME = "Dúo";
